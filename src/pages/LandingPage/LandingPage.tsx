fb970e302388fecf355e9a9285ac7ed5e0fda636
vinicius webster <viniciuswebster99@gmail.com>
2023-11-11 14:30:56 -0300

    style: size adjusts

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -4,26 +4,46 @@ import landingImg from "../../assets/landing-img.png";
 import { StyledContainer } from "../../UI/utils/container";
 
 const StyledDiv = styled.div`
-  flex:1;
   display: flex;
-  gap: 32px;
+  align-items: center;
   min-height: 100%;
-  justify-content: space-between;
-  max-width: 1500px;
-  margin: auto;
+  padding: 0 32px;
+  justify-content: space-around;
+  width: 100%;
+
+  @media screen and (max-width: 1300px) {
+    figure {
+      width: 40%;
+      img {
+        width: 100%;
+      }
+    }
+    gap: 64px;
+    justify-content: space-around;
+  }
+  @media screen and (max-width: 890px) {
+    figure {
+      width: 100%;
+      img {
+        width: 100%;
+      }
+    }
+    gap: 64px;
+    flex-direction: column;
+  }
 `;
 
 const LandingPage = () => {
   return (
     <StyledContainer>
-      <StyledDiv>
-        <figure>
-          <img src={logo} alt="cheiro de novo" />
-        </figure>
-        <figure>
-          <img src={landingImg} alt="Moça sentada olhando para o lado" />
-        </figure>
-      </StyledDiv>
+        <StyledDiv>
+          <figure>
+            <img src={logo} alt="cheiro de novo" />
+          </figure>
+          <figure>
+            <img src={landingImg} alt="Moça sentada olhando para o lado" />
+          </figure>
+        </StyledDiv>
     </StyledContainer>
   );
 };
