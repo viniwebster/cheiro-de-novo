207407d830880efa1f5a58cdc206b6bb20c96235
vinicius webster <viniciuswebster99@gmail.com>
2023-11-11 14:31:40 -0300

    refactor: add new sections

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,12 +1,17 @@
+import BestProducts from "../../components/BestProducts/BestProducts"
+import Infos from "../../components/Infos/Infos"
 import BannerSwiper from "../../components/Swiper/Swiper"
+import Title from "../../components/Title/Title"
 import LandingPage from "../LandingPage/LandingPage"
-
 
 const HomePage = () => {
   return(
     <>
       <LandingPage />
       <BannerSwiper />
+      <Title text="Destaques"/>
+      <BestProducts />
+      <Infos />
     </>
   )
 }
