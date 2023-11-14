7877266c340d7a77d0c1a2a881c27cad8206c50c
vinicius webster <viniciuswebster99@gmail.com>
2023-11-14 13:07:38 -0300

    feat: interface user

---
+++ src/interfaces/IUser.ts
@@ -0,0 +1,12 @@
+export interface IUser {
+  name: string
+  surname: string
+  email: string
+  password: string
+  user: string
+  cep: string
+  street: string
+  number: string
+  city: string
+  phone: string
+}
