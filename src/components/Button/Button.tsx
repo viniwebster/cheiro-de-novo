0ad31fd33a1fb3b52679b6eed69e20410cd013e0
vinicius webster <viniciuswebster99@gmail.com>
2023-11-14 11:39:54 -0300

    feat: add onClick function

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,26 +2,30 @@ import styled from "styled-components"
 import { veryDarkColor, white } from "../../UI/variables"
 
 const StyledButton = styled.button`
-  border-radius: 20px;
+  border-radius: 30px;
   background-color: ${veryDarkColor};
   color: ${white};
   font-weight: 800;
   border: none;
   padding: 10px 16px;
-  min-width: 100px;
+  min-width: 150px;
+  height: 50px;
 
   &:hover{
     background-color: #4b8974;
+    transition: .3s;
+    cursor: pointer;
   }
 `
 
 interface Props{
   text: string
+  onClick?: () => void
 }
 
-const Button = ({ text } : Props) => {
+const Button = ({ text, onClick } : Props) => {
   return(
-    <StyledButton>
+    <StyledButton onClick={onClick}>
       {text}
     </StyledButton>
   )
