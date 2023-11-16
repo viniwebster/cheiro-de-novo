8034767a255af30758e2e23f2fa8e9a2f3fb940a
vinicius webster <viniciuswebster99@gmail.com>
2023-11-16 15:56:30 -0300

    fix: type

--- src/components/InputSearch/InputSearch.tsx
+++ src/components/InputSearch/InputSearch.tsx
@@ -7,23 +7,30 @@ const StyledInput = styled.input`
   border: 1px solid ${white};
   border-radius: 30px;
   padding: 10px 16px;
-  width: 350px;
+  width: 100%;
   box-sizing: border-box;
   color: ${white};
 
   &::placeholder {
     color: ${white};
   }
-`
+`;
 
 interface Props {
-  placeholder: string
+  value: string;
+  placeholder: string;
+  setSearch: React.Dispatch<React.SetStateAction<string>>;
 }
 
-const InputSearch = ({ placeholder }: Props) => {
-  return(
-    <StyledInput placeholder={placeholder}/>
-  )
-}
+const InputSearch = ({ placeholder, setSearch, value }: Props) => {
+  return (
+    <StyledInput
+      placeholder={placeholder}
+      onChange={(event) => setSearch(event.target.value)}
+      value={value}
+      type="search"
+    />
+  );
+};
 
-export default InputSearch
+export default InputSearch;
