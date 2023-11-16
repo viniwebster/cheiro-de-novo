7481421adcd5b4ff9c5d641be68218344c5dfa33
vinicius webster <viniciuswebster99@gmail.com>
2023-11-16 15:51:39 -0300

    feat: item search component

---
+++ src/components/ItemSearch/ItemSearch.tsx
@@ -0,0 +1,54 @@
+import styled from "styled-components"
+import { darkColor } from "../../UI/variables"
+import { Link } from "react-router-dom"
+
+const StyledLink = styled(Link)`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 16px;
+  width: 100%;
+  background-color: white;
+  text-decoration: none;
+  gap: 24px;
+
+  img {
+    width: 100px;
+    height: 100px;
+    object-fit: contain;
+  }
+
+  span {
+    font-weight: 800;
+    color: ${darkColor};
+  }
+
+  h3 {
+    font-weight: 600;
+    color: ${darkColor};
+  }
+
+  &:hover {
+    transition: .3s;
+    background-color: #eef5ec;
+  }
+`
+
+interface Props {
+  img: string,
+  title: string,
+  price: number,
+  id: number
+}
+
+const ItemSearch = ({ img, title, price, id } : Props) => {
+  return(
+    <StyledLink to={`/product/${id}`}>
+      <img src={img} alt={title} />
+      <h3>{title}</h3>
+      <span>R$ {price}</span>
+    </StyledLink>
+  )
+}
+
+export default ItemSearch
