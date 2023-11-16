c2548e505eec9dbc520069a07896f418ef375815
vinicius webster <viniciuswebster99@gmail.com>
2023-11-16 15:59:36 -0300

    feat: new routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,33 @@ import { GlobalStyles } from "./UI/GlobalStyles";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import DefaultPage from "./pages/DefaultPage/DefaultPage";
 import HomePage from "./pages/HomePage/HomePage";
+import Login from "./pages/Login/Login";
+import Product from "./pages/Product/Product";
+import { QueryClient, QueryClientProvider } from "react-query";
+import Products from "./pages/Products/Products";
+import SingUp from "./pages/SingUp/SingUp";
+import { RecoilRoot } from "recoil";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <BrowserRouter>
-    <GlobalStyles />
-      <Routes>
-        <Route path="/" element={<DefaultPage />}>
-          <Route index element={<HomePage />} />
-          <Route path="/login" element={<h1>Login</h1>} />
-          <Route path="/products" element={<h1>Products</h1>} />
-        </Route>
-      </Routes>
+      <GlobalStyles />
+      <RecoilRoot>
+        <QueryClientProvider client={queryClient}>
+          <Routes>
+            <Route path="/" element={<DefaultPage />}>
+              <Route index element={<HomePage />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/singup" element={<SingUp />} />
+              <Route path="/products" element={<Products />} />
+              <Route path="/product/:id" element={<Product />} />
+              <Route path="/account" element={<h1>Minha conta</h1>} />
+            </Route>
+          </Routes>
+        </QueryClientProvider>
+      </RecoilRoot>
     </BrowserRouter>
   );
 }
