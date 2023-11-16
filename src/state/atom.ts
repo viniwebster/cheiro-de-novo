1f145ac3939a7da0edb038f29d8a7be66044641e
vinicius webster <viniciuswebster99@gmail.com>
2023-11-16 15:58:00 -0300

    refactor: add new state from cart

--- src/state/atom.ts
+++ src/state/atom.ts
@@ -1,6 +1,12 @@
 import { atom } from "recoil"
+import { IProduct } from "../interfaces/IProduct"
 
 export const userState = atom({
   key: 'userState',
   default: false
+})
+
+export const useCart = atom<IProduct[]>({
+  key: 'useCart',
+  default: []
 })
