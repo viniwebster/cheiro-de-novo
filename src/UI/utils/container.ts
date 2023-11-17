6b83b045a3b431f1cc4a6b4d27bc19e4b9b48e6a
vinicius webster <viniciuswebster99@gmail.com>
2023-11-17 14:30:35 -0300

    style: remove padding

--- src/UI/utils/container.ts
+++ src/UI/utils/container.ts
@@ -1,15 +1,26 @@
 import styled from "styled-components";
-import ramo from "../../assets/background/ramo.png"
-import ramo2 from "../../assets/background/ramo2.png"
+import ramo from "../../assets/background/ramo.png";
+import ramo2 from "../../assets/background/ramo2.png";
 
 export const StyledContainer = styled.section`
   padding: 62px 0;
   margin: auto;
   display: flex;
-  align-items: center;
   min-height: calc(100vh - 90px);
 
-  background: no-repeat right bottom/10% url(${ramo}),
-  no-repeat left bottom/10% url(${ramo2})
-  ;
-`
+  background: no-repeat right bottom/7% url(${ramo}),
+    no-repeat left bottom/5% url(${ramo2});
+
+  @media screen and (max-width: 500px) {
+    background: no-repeat right bottom/20% url(${ramo}),
+      no-repeat left bottom/20% url(${ramo2});
+  }
+`;
+
+export const Container = styled.div`
+  width: 100%;
+  max-width: 1200px;
+  margin: auto;
+  /* padding: 0 32px; */
+  box-sizing: border-box;
+`;
