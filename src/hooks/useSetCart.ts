a4a40b2e7437bf8fbff49ddfb56c4217818bf5ec
viniwebster <viniciuswebster99@gmail.com>
2024-05-19 15:12:16 -0300

    refactor: add buyId for purchases

--- src/hooks/useSetCart.ts
+++ src/hooks/useSetCart.ts
@@ -1,10 +1,11 @@
 import { useSetRecoilState } from "recoil";
 import { useCart } from "../state/atom";
 import { IProduct } from "../interfaces/IProduct";
+import {v4 as uuidv4} from "uuid";
 
 export const useSetCart = () => {
   const setCartList = useSetRecoilState<IProduct[]>(useCart);
   return (product: IProduct) => {
-    setCartList((items) => [...items, product]);
+    setCartList((items) => [...items, {...product, buyId: uuidv4()}]);
   };
 };
